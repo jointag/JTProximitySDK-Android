a7e452ac9eeb49d16532544296edbb0c024f7b51
Marco Fraccaroli <marco.fraccaroli@mwd.digital>
2018-07-24 11:31:15 +0200

    Added missing checkPendingPermissions call in MainActivity onRequestPermissionsResult

--- Examples/Empty/app/src/main/java/com/jointag/proximity/examples/empty/ui/MainActivity.java
+++ Examples/Empty/app/src/main/java/com/jointag/proximity/examples/empty/ui/MainActivity.java
@@ -3,12 +3,16 @@ package com.jointag.proximity.examples.empty.ui;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.jointag.proximity.ProximitySDK;
 import com.jointag.proximity.examples.empty.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +20,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            ProximitySDK.getInstance().checkPendingPermissions();
+        }
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 }
